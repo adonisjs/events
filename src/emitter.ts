d9202b61444805769eb9d7103ba50c4549fb70fc
Harminder Virk <virk.officials@gmail.com>
2022-12-12 08:54:58 +0530

    chore: update dependencies

--- src/emitter.ts
+++ src/emitter.ts
@@ -52,9 +52,9 @@ export class Emitter<EventsList extends Record<string | symbol | number, any>> {
    * Reference to AdonisJS application, we need the application root
    * and container reference from it.
    */
-  #app: Application<any, any, any>
+  #app: Application<any, any>
 
-  constructor(app: Application<any, any, any>) {
+  constructor(app: Application<any, any>) {
     this.#app = app
   }
 
