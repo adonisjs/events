8cb78999013d360c5917d98752a9775d61ec06c6
Harminder Virk <virk.officials@gmail.com>
2023-01-26 23:53:14 +0530

    refactor: expose all factories from the factories sub-path

---
+++ test_factories/main.ts
@@ -0,0 +1,10 @@
+/*
+ * @adonisjs/events
+ *
+ * (c) AdonisJS
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+export { EmitterFactory } from './emitter.js'
